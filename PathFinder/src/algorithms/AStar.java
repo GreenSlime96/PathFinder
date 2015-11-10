cc4aa8f4794e95449b65cb835bd00daf8361da3f
Kheng Boon Pek <boonpek96@gmail.com>
2015-11-10 00:51:45 +0000

    safety checks for Node heuristic, will remove next commit

--- PathFinder/src/algorithms/AStar.java
+++ PathFinder/src/algorithms/AStar.java
@@ -1,7 +1,9 @@
 package algorithms;
 
 import java.awt.Point;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.Stack;
@@ -14,6 +16,8 @@ public class AStar {
 	// TODO: the whole thing is incomplete! :(
 	public static final void search(Search search) {
 		State startState = search.getStartState();
+		
+		Map<Point, Double> testMap = new HashMap<Point, Double>();
 
 		Stack<Point> solution = search.getSolution();
 		Set<Point> opened = search.getOpened();
@@ -50,30 +54,41 @@ public class AStar {
 			}
 
 			List<Node> nodes = search.expand(node);
-
+			
 			for (Node n : nodes) {
 				Point p = n.getState().getStart();
 
 				if (closed.contains(p))
 					continue;
-
-//				double ng = node.getDepth() + ((p.x - point.x == 0 || p.y - point.y == 0) ? 1 : Math.sqrt(2));			
 				
-				if (!opened.contains(p)) {
-					opened.add(p);
-					queue.add(n);
+	            // get the distance between current node and the neighbor
+	            // and calculate the next g score
+	            double ng = node.getDepth() + ((p.x - point.x == 0 || p.y - point.y == 0) ? 1 : Math.sqrt(2));
+
+				if (!opened.contains(p) || ng < n.getDepth()) {
+
+					if (!opened.contains(p)) {
+						testMap.put(p, n.getHeuristic());
+						opened.add(p);
+						queue.add(n);
+					} else {
+						System.out.println("we here bro");
+					}
+
+
 				}
 				
-//				if (!opened.contains(p) || ng < n.getDepth()) {
-//					if (!opened.contains(p)) {
-//						opened.add(p);
-//						queue.add(n);
-//					} else {
-//						System.out.println("we are at this weird place...\t" + p);
-////						queue.remove(n);
-////						queue.add(new Node(n.getParent(), n.getState(), n.getDepth(), ));
-//					}
-//				}
+				if (n.getHeuristic() < testMap.get(p)) {
+					System.out.println("hmmm");
+					testMap.put(p, n.getHeuristic());
+				}
+			}
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 
